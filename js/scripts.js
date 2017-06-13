a9059aba5bc5d769430216e77908f1cf7d15595c
Dylan Lewis and Max Scher <max@>
2017-06-13 16:24:07 -0700

    add name and greeting to balance output

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // business logic //
-function BankAccount(name, initialDeposit) {
-  this.name = name;
+function BankAccount(userName, initialDeposit) {
+  this.userName = userName;
   this.initialDeposit = initialDeposit;
   this.balance = initialDeposit;
 };
@@ -25,17 +25,18 @@ BankAccount.prototype.withdrawalCash = function (withdrawal) {
 
 // user logic //
 $(document).ready(function() {
-  var initialDeposit;
+  // var initialDeposit;
   var userName;
-  var newBankAccount;
-  var deposit;
-  var withdrawal;
-  var revBankAccount;
+  // var newBankAccount;
+  // var deposit;
+  // var withdrawal;
+  // var revBankAccount;
   $("#register").submit(function(event) {
     event.preventDefault();
     initialDeposit = parseInt($("#init-deposit").val());
-    userName = $("#name").val();
+    userName = $("#person").val();
     newBankAccount = new BankAccount(userName, initialDeposit);
+    $("#user").text(userName);
     $("#balance").text(newBankAccount.initialDeposit);
     $(".output").show();
     console.log(newBankAccount);
@@ -44,6 +45,7 @@ $(document).ready(function() {
     event.preventDefault();
     deposit = parseInt($("#deposit").val());
     withdrawal = parseInt($("#withdrawal").val());
+    $("#user").text(userName);
     $("#balance").text(newBankAccount.depositCash(deposit));
     $("#balance").text(newBankAccount.withdrawalCash(withdrawal));
     console.log(newBankAccount);
